f97c1d3300d60fdf5f2a740b467706e3ea28b48e
Kasi Kalidindi <kasi.kalidindi@gmail.com>
2018-11-05 04:55:40 +0000

    Contract Upgrade to CDT 1.3.2

--- drtoken/drtoken.cpp
+++ drtoken/drtoken.cpp
@@ -2,26 +2,25 @@
  *  @file
  *  @copyright defined in eos/LICENSE.txt
  */
-
 #include "drtoken.hpp"
 
 namespace ampersand {
 
-void drtoken::create(account_name issuer,
+void drtoken::create(name issuer,
                      asset maximum_supply,
                      bool transfer_locked)
 {
-    require_auth(_self);
-    require_auth2(N(amprllc), N(create));
+    require_auth(_code);
+//    require_auth2(name("amprllc"), name("create"));
     
     auto sym = maximum_supply.symbol;
     eosio_assert(sym.is_valid(), "invalid symbol name");
     eosio_assert(maximum_supply.is_valid(), "invalid supply");
     eosio_assert(maximum_supply.amount > 0, "max-supply must be positive");
 
-    stats statstable(_self, sym.name());
-    auto existing = statstable.find(sym.name());
-    eosio_assert(existing == statstable.end(), "token with symbol already exists ");
+    stats statstable(_code, sym.raw());
+    auto existing = statstable.find(sym.raw());
+    eosio_assert(existing == statstable.end(), "token with symbol already exists");
 
     statstable.emplace(_self, [&](auto& s) {
         s.supply.symbol = maximum_supply.symbol;
@@ -31,98 +30,103 @@ void drtoken::create(account_name issuer,
     });
 }
 
-
-void drtoken::issue(account_name to, 
-                    asset quantity, 
+void drtoken::issue(name to,
+                    asset quantity,
                     string memo)
 {
     auto sym = quantity.symbol;
     eosio_assert(sym.is_valid(), "invalid symbol name");
     eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
 
-    auto sym_name = sym.name();
-    stats statstable(_self, sym_name);
-    auto existing = statstable.find(sym_name);
-    eosio_assert( existing != statstable.end(), 
+    stats statstable(_code, sym.raw());
+
+    auto existing = statstable.find(sym.raw());
+    eosio_assert( existing != statstable.end(),
                   "token with symbol does not exist, create token before issue");
     const auto& st = *existing;
 
     require_auth(st.issuer);
-    require_auth2(N(amprllc), N(issuer));
+//    require_auth2(name("amprllc"), name("issue"));
 
     eosio_assert(quantity.is_valid(), "invalid quantity");
     eosio_assert(quantity.amount > 0, "must issue positive quantity");
-
     eosio_assert(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
-    eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount, 
+    eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount,
                   "quantity exceeds available supply");
 
-    statstable.modify(st, 0, [&](auto& s) {
+    statstable.modify(st, same_payer, [&](auto& s) {
         s.supply += quantity;
     });
 
     add_balance(st.issuer, quantity, st.issuer);
 
     if(to != st.issuer) {
-        SEND_INLINE_ACTION( *this, transfer, {st.issuer,N(active)}, {st.issuer, to, quantity, memo} );
+        SEND_INLINE_ACTION( *this, transfer, {st.issuer,name("active")}, {st.issuer, to, quantity, memo} );
     }
 }
 
-void drtoken::unlock(asset unlock) 
+void drtoken::unlock(asset unlock)
 {
     eosio_assert(unlock.symbol.is_valid(), "invalid symbol name");
     eosio_assert(unlock.is_valid(), "invalid supply");
 
-    auto symbol_name = unlock.symbol.name();
-    stats statstable(_self, symbol_name);
+    auto symbol_code = unlock.symbol.raw();
+    stats statstable(_code, symbol_code);
 
-    auto token = statstable.find(symbol_name);
+    auto token = statstable.find(symbol_code);
     eosio_assert(token != statstable.end(), "token with the symbol doesn't exist");
+
     const auto &stats_record = *token;
 
+    // authorization from issuer@issuer is needed eg: amprllc@issuer
+    //require_auth((stats_record.issuer, name("issuer"));
     require_auth(stats_record.issuer);
-    require_auth2(N(amprllc), N(unlock));
 
-    statstable.modify(stats_record, 0, [&](auto& srec) {
+    statstable.modify(stats_record, same_payer, [&](auto& srec) {
         srec.transfer_locked = false;
     });
 }
 
-void drtoken::transfer(account_name from,
-                       account_name to,
+void drtoken::transfer(name from,
+                       name to,
                        asset quantity,
                        string memo )
 {
     eosio_assert(from != to, "cannot transfer to self");
+
     require_auth(from);
-    eosio_assert(is_account( to ), "to account does not exist");
-    auto sym = quantity.symbol.name();
-    stats statstable(_self, sym);
-    const auto& st = statstable.get(sym);
 
-    eosio_assert(st.transfer_locked==true,"token transfer is locked");
+    eosio_assert(is_account(to), "to account does not exist");
 
-    require_recipient(from);
-    require_recipient(to);
+    auto sym = quantity.symbol;
+    stats statstable(_code, sym.raw());
+
+    const auto& st = statstable.get(sym.raw());
+
+    if (st.transfer_locked) {
+        require_auth(st.issuer);
+    }
+
+//    require_recipient(from);
+//    require_recipient(to);
 
     eosio_assert(quantity.is_valid(), "invalid quantity");
     eosio_assert(quantity.amount > 0, "must transfer positive quantity");
     eosio_assert(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
     eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
 
-
     sub_balance(from, quantity);
     add_balance(to, quantity, from);
 }
 
-void drtoken::sub_balance(account_name owner, 
-                          asset value) 
+void drtoken::sub_balance(name owner,
+                          asset value)
 {
-    accounts from_acnts(_self, owner);
+    accounts from_acnts(_self, owner.value);
 
-    const auto& from = from_acnts.get(value.symbol.name(), "no balance object found");
+    const auto& from = from_acnts.get(value.symbol.raw(), "no balance object found");
+
     eosio_assert(from.balance.amount >= value.amount, "overdrawn balance");
-
 
     if(from.balance.amount == value.amount) {
        from_acnts.erase(from);
@@ -133,16 +137,18 @@ void drtoken::sub_balance(account_name owner,
    }
 }
 
-void drtoken::add_balance(account_name owner, asset value, account_name ram_payer)
+void drtoken::add_balance(name owner, asset value, name ram_payer)
 {
-    accounts to_acnts(_self, owner);
-    auto to = to_acnts.find(value.symbol.name());
+    accounts to_acnts(_self, owner.value);
+
+    auto to = to_acnts.find(value.symbol.raw());
+
     if(to == to_acnts.end()) {
        to_acnts.emplace(ram_payer, [&](auto& a){
             a.balance = value;
         });
     } else {
-        to_acnts.modify(to, 0, [&](auto& a) {
+        to_acnts.modify(to, same_payer, [&](auto& a) {
             a.balance += value;
         });
     }
@@ -150,4 +156,4 @@ void drtoken::add_balance(account_name owner, asset value, account_name ram_paye
 
 } /// namespace ampersand
 
-EOSIO_ABI(ampersand::drtoken, (create)(issue)(transfer))
+EOSIO_DISPATCH(ampersand::drtoken, (create)(issue)(unlock)(transfer))
--- drtoken/drtoken.hpp
+++ drtoken/drtoken.hpp
@@ -4,8 +4,8 @@
  */
 #pragma once
 
-#include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
 
 #include <string>
 
@@ -15,80 +15,79 @@ namespace ampersand {
 
     using std::string;
 
-    class drtoken : public contract {
+    CONTRACT drtoken : public eosio::contract {
 
     public:
-        
-        drtoken(account_name self):contract(self){}
+	using contract::contract;
 
-        // token creation with transfer_locked state    
         [[eosio::action]]
-        void create(account_name issuer,
+        void create(name issuer,
                     asset maximum_supply,
                     bool transfer_locked);
 
-        // issue the created tokens
         [[eosio::action]]
-        void issue(account_name to, asset quantity, string memo);
+        void issue(name to, asset quantity, string memo);
 
-        /// unlock the transfer state of the asset being created
         [[eosio::action]]
         void unlock(asset unlock);
 
-        // transfer tokens
         [[eosio::action]]
-        void transfer(account_name from,
-                      account_name to,
+        void transfer(name from,
+                      name to,
                       asset quantity,
                       string memo);
-      
-        inline asset get_supply(symbol_name sym)const;
-         
-        inline asset get_balance(account_name owner, symbol_name sym)const;
+
+        inline asset get_supply(symbol sym)const;
+
+        inline asset get_balance(name owner, symbol sym)const;
 
     private:
-        // blockchain db structure to store account vs balance of tokens
-        struct [[eosio::table]] account {
+
+        struct [[eosio::table]]  account {
             asset balance;
-            uint64_t primary_key()const { return balance.symbol.name(); }
+
+            uint64_t primary_key()const { return balance.symbol.raw(); }
         };
 
-        // blockchain db structure to store token stats 
         struct [[eosio::table]] currency_stats {
             asset supply;
             asset max_supply;
-            account_name issuer;
+            name issuer;
             bool transfer_locked;
-            uint64_t primary_key()const { return supply.symbol.name(); }
+
+            uint64_t primary_key()const { return supply.symbol.raw(); }
         };
 
-        typedef eosio::multi_index<N(accounts), account> accounts;
-        typedef eosio::multi_index<N(stats), currency_stats> stats;
+        typedef eosio::multi_index<"accounts"_n, account> accounts;
+        typedef eosio::multi_index<"stats"_n, currency_stats> stats;
 
-        void sub_balance( account_name owner, asset value );
-        void add_balance( account_name owner, asset value, account_name ram_payer );
+        void sub_balance( name owner, asset value );
+        void add_balance( name owner, asset value, name ram_payer );
 
     public:
+
         struct transfer_args {
-            account_name from;
-            account_name to;
+            name from;
+            name to;
             asset quantity;
             string memo;
         };
+
     };
 
-    asset drtoken::get_supply( symbol_name sym )const
+    asset drtoken::get_supply( symbol sym )const
     {
-        stats statstable( _self, sym );
-        const auto& st = statstable.get( sym );
+        stats statstable(_code, sym.raw());
+        const auto& st = statstable.get(sym.raw());
          return st.supply;
     }
 
-    asset drtoken::get_balance( account_name owner, symbol_name sym )const
+    asset drtoken::get_balance(name owner, symbol sym)const
     {
-        accounts accountstable( _self, owner );
-        const auto& ac = accountstable.get( sym );
+        accounts accountstable(_code, owner.value);
+        const auto& ac = accountstable.get( sym.raw());
         return ac.balance;
     }
 
 } /// namespace ampersand
+
