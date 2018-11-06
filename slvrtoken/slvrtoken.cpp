483ff5dfad7999b578bbaa040117b85389199394
Kasi Kalidindi <kasi.kalidindi@gmail.com>
2018-11-06 10:50:34 +0000

    Initial check-in of slvrtoken contract.

---
+++ slvrtoken/slvrtoken.cpp
@@ -0,0 +1,201 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include "slvrtoken.hpp"
+
+namespace ampersand {
+
+void slvrtoken::create(name issuer,
+                     asset new_supply,
+                     bool transfer_locked)
+{
+    require_auth(_code);
+//    require_auth2(name("amprllc"), name("create"));
+    
+    auto sym = new_supply.symbol;
+    eosio_assert(sym.is_valid(), "invalid symbol name");
+    eosio_assert(new_supply.is_valid(), "invalid supply");
+    eosio_assert(new_supply.amount > 0, "max-supply must be positive");
+    stats statstable(_code, sym.raw());
+
+    auto iterator = statstable.find(sym.raw());
+
+    // Token is added for the first time
+    if (iterator == statstable.end()){	
+        statstable.emplace(_code, [&](auto& token_stats) {
+            token_stats.supply.symbol = new_supply.symbol;
+            token_stats.total_supply = new_supply;
+            token_stats.issuer = issuer;
+            token_stats.transfer_locked = transfer_locked;
+        });
+    // Token Already exists, reissuing with new supply
+    } else {
+        statstable.modify(iterator, same_payer, [&](auto& token_stats_record) {
+            token_stats_record.supply.symbol = new_supply.symbol;
+            token_stats_record.total_supply += new_supply;
+            token_stats_record.issuer = issuer;
+            token_stats_record.transfer_locked = transfer_locked;
+        });
+    }
+}
+
+void slvrtoken::issue(name to,
+                    asset quantity,
+                    string memo)
+{
+    auto sym = quantity.symbol;
+    eosio_assert(sym.is_valid(), "invalid symbol name");
+    eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
+
+    stats statstable(_code, sym.raw());
+
+    auto iterator = statstable.find(sym.raw());
+    eosio_assert( iterator != statstable.end(),
+                  "token with symbol does not exist, create token before issue");
+
+    require_auth(iterator->issuer);
+//    require_auth2(name("amprllc"), name("issue"));
+
+    eosio_assert(quantity.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "must issue positive quantity");
+    eosio_assert(quantity.symbol == iterator->supply.symbol, "symbol precision mismatch");
+    eosio_assert( quantity.amount <= iterator->total_supply.amount - iterator->supply.amount,
+                  "quantity exceeds available supply");
+
+    statstable.modify(iterator, same_payer, [&](auto& token_stats_record) {
+        token_stats_record.supply += quantity;
+    });
+
+    add_balance(iterator->issuer, quantity, iterator->issuer);
+
+    if(to != iterator->issuer) {
+        SEND_INLINE_ACTION( *this, 
+                            transfer, 
+                            {iterator->issuer, name("active")},
+                            {iterator->issuer, to, quantity, memo} );
+    }
+}
+
+void slvrtoken::unlock(asset unlock)
+{
+    eosio_assert(unlock.symbol.is_valid(), "invalid symbol name");
+    eosio_assert(unlock.is_valid(), "invalid supply");
+
+    auto symbol_code = unlock.symbol.raw();
+    stats statstable(_code, symbol_code);
+
+    auto iterator = statstable.find(symbol_code);
+    eosio_assert(iterator != statstable.end(), "token with the symbol doesn't exist");
+
+    // authorization from issuer@issuer is needed eg: amprllc@issuer
+    //require_auth((stats_record.issuer, name("issuer"));
+    require_auth(iterator->issuer);
+
+    statstable.modify(iterator, same_payer, [&](auto& token_stats_record) {
+        token_stats_record.transfer_locked = false;
+    });
+}
+
+void slvrtoken::transfer(name from,
+                       name to,
+                       asset quantity,
+                       string memo )
+{
+    eosio_assert(from != to, "cannot transfer to self");
+
+    require_auth(from);
+
+    eosio_assert(is_account(to), "to account does not exist");
+
+    auto sym = quantity.symbol;
+    stats statstable(_code, sym.raw());
+
+    eosio_assert(statstable.find(quantity.symbol.raw()) != statstable.end(), "token with the symbol doesn't exist");
+
+    const auto& token_stats_record = statstable.get(sym.raw());
+
+    if (token_stats_record.transfer_locked) {
+        require_auth(token_stats_record.issuer);
+    }
+
+//    require_recipient(from);
+//    require_recipient(to);
+
+    eosio_assert(quantity.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "must transfer positive quantity");
+    eosio_assert(quantity.symbol == token_stats_record.supply.symbol, "symbol precision mismatch");
+    eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
+
+    sub_balance(from, quantity);
+    add_balance(to, quantity, from);
+}
+
+void slvrtoken::redeem(name owner,
+                       asset quantity)
+{
+eosio::print("***redeem called***");
+    accounts accounts_table(_code, owner.value);
+    const auto& account_object = accounts_table.get(quantity.symbol.raw(), "no balance object found");
+
+    eosio_assert(account_object.balance.amount >= quantity.amount, "can't redeem more tokens than have");
+
+    require_auth(owner);
+
+eosio::print("current balance: ", account_object.balance);
+    if(account_object.balance.amount == quantity.amount) {
+eosio::print("token balance zero");
+        accounts_table.erase(account_object);
+    } else {
+        accounts_table.modify(account_object, owner, [&](auto & obj) {
+            obj.balance -= quantity;
+eosio::print("new balance: ", obj.balance);
+        });
+    }
+
+    action(
+        permission_level{_code, name("active")},
+        name("drtokenac"), name("transfer"),
+        std::make_tuple(name("drtokenac"), owner, "10.0000 ADRT",std::string(""))
+    ).send();
+
+}
+
+void slvrtoken::sub_balance(name owner,
+                            asset value)
+{
+    accounts from_acnts(_code, owner.value);
+
+    const auto& from = from_acnts.get(value.symbol.raw(), "no balance object found");
+
+    eosio_assert(from.balance.amount >= value.amount, "overdrawn balance");
+
+    if(from.balance.amount == value.amount) {
+       from_acnts.erase(from);
+    } else {
+        from_acnts.modify(from, owner, [&](auto& a) {
+            a.balance -= value;
+        });
+   }
+}
+
+void slvrtoken::add_balance(name owner, asset value, name ram_payer)
+{
+    accounts to_acnts(_code, owner.value);
+
+    auto to = to_acnts.find(value.symbol.raw());
+
+    if(to == to_acnts.end()) {
+       to_acnts.emplace(ram_payer, [&](auto& a){
+            a.balance = value;
+        });
+    } else {
+        to_acnts.modify(to, same_payer, [&](auto& a) {
+            a.balance += value;
+        });
+    }
+}
+
+} /// namespace ampersand
+
+EOSIO_DISPATCH(ampersand::slvrtoken, (create)(issue)(unlock)(redeem)(transfer))
---
+++ slvrtoken/slvrtoken.hpp
@@ -0,0 +1,101 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#pragma once
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+
+#include <string>
+
+using namespace eosio;
+
+namespace ampersand {
+
+    using std::string;
+
+    CONTRACT slvrtoken : public eosio::contract {
+
+    public:
+	using contract::contract;
+
+        [[eosio::action]]
+        void create(name issuer,
+                    asset maximum_supply,
+                    bool transfer_locked);
+
+        [[eosio::action]]
+        void issue(name to, asset quantity, string memo);
+
+        [[eosio::action]]
+        void unlock(asset unlock);
+
+        [[eosio::action]]
+        void transfer(name from,
+                      name to,
+                      asset quantity,
+                      string memo);
+
+        [[eosio::action]]
+        void redeem(name owner,
+                    asset quantity);
+
+        inline asset get_supply(symbol sym)const;
+
+        inline asset get_balance(name owner, symbol sym)const;
+
+    private:
+
+        struct [[eosio::table]]  account {
+            asset balance;
+
+            uint64_t primary_key()const { return balance.symbol.raw(); }
+
+            EOSLIB_SERIALIZE(account, (balance))
+        };
+
+        struct [[eosio::table]] currency_stats {
+            asset supply;
+            asset total_supply;
+            name issuer;
+            bool transfer_locked;
+
+            uint64_t primary_key()const { return supply.symbol.raw(); }
+
+            EOSLIB_SERIALIZE(currency_stats, (supply)(total_supply)(issuer)(transfer_locked))
+        };
+
+        typedef eosio::multi_index<"accounts"_n, account> accounts;
+        typedef eosio::multi_index<"stats"_n, currency_stats> stats;
+
+        void sub_balance( name owner, asset value );
+        void add_balance( name owner, asset value, name ram_payer );
+
+    public:
+
+        struct transfer_args {
+            name from;
+            name to;
+            asset quantity;
+            string memo;
+        };
+
+    };
+
+    asset slvrtoken::get_supply( symbol sym )const
+    {
+        stats statstable(_code, sym.raw());
+        const auto& st = statstable.get(sym.raw());
+         return st.supply;
+    }
+
+    asset slvrtoken::get_balance(name owner, symbol sym)const
+    {
+        accounts accountstable(_code, owner.value);
+        const auto& ac = accountstable.get( sym.raw());
+        return ac.balance;
+    }
+
+} /// namespace ampersand
+
